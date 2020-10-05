3c4e3020fa37fca4f184239cfbe9961272035217
EricChiou <miatlab@gmail.com>
2020-10-05 23:36:11 +0800

    update commit

--- matchstring/matchstring.go
+++ matchstring/matchstring.go
@@ -4,7 +4,7 @@ import (
 	"regexp"
 )
 
-// Match09AZ match string 0-9, a-z
+// Match09AZ match string 0-9, a-z, A-Z
 func Match09azAZ(s string) (bool, error) {
 	match, err := regexp.MatchString("^[0-9a-zA-Z]+$", s)
 	return match, err
